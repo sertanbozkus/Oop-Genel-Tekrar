a5f21371e796f8a05b9fb417d4336fe28a9840c3
sertanbozkus <sertan.bozkus@bilgeadam.com>
2024-02-15 02:56:33 +0300

    Add project files.

---
+++ Oop-Genel-Tekrar/Program.cs
@@ -0,0 +1,15 @@
+﻿// Telefon -> Üretim Tarihi, Id, Ad, Açıklama, İşletim Sistemi, Lisanslı olup olmama bilgisi ( Kayıt anında üretim tarihi atansın )
+
+// Bilgisayar -> Üretim Tarihi, Id, Ad, Açıklama, İşletim Sistemi Usb Giriş Sayısı, Bluetooth olup olmama bilgisi ( Kayıt anında üretim tarihi atansın )
+
+// Bir BaseClass (Makine) kullanılarak Inheritance yapılacak. (Inheritance)
+
+// Ek -> Usb giriş sayısı 2 veya 4 atanabilir, aksi durumda -1 olarak atansın. (Encapsulation)
+
+// Üretim Tarihi, Id, Ad , Açıklama , İşletim sistemi bilgilerinin yazdırıldığı ortak bir metot tanımlansın. Bu metot Telefonda ve Bilgisayar'da ezilerek bütün özellikleri yazdırsın. (Virtual - Polymorphism)
+
+// UrunAdiGetir diye abstract bit metot Bilgisayar için "Bilgisayarınızın adı ---> .... " , Telefon için "Telefonunuzun adı ----> .... " (Abstraction)
+
+// Ardından program.cs'te kullanıya Bilgisayar eklemek için 1 , Telefon eklemek için 2'ye basması gerektiğini söyleyiniz.
+
+// Kullanıcıdan gelen talebe göre ekleme işlemini gerçekleştiriniz, kullanıcı başka ürün eklemek istemezse bütün bilgisayar ve telefon bilgilerini ekrana yazdırınız.
